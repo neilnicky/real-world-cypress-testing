dc5822ad7a099c8540fb75cfe9768a9496372109
neilnicky <neilnicholaskanat@gmail.com>
2025-02-14 18:44:51 +0530

    test: only,/caseinsensitive/i

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,13 @@
 describe("home page", () => {
   it("the h1 contains the correct text", () => {
     cy.visit("http://localhost:3000")
-    cy.get("h1").contains("Testing Next.js Applications with Cypress")
+    cy.get("[data-test='hero-heading']").contains(
+      "Testing Next.js Applications with Cypress"
+    )
+  })
+
+  it.only("the features on the homepage are correct", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(0).contains("4 Courses")
   })
 })
